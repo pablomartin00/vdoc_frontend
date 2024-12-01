35fe3a009ce52f69c96c71d82f9735fd4bfaee26
Pablo Martin <pablomartin00@hotmail.com>
2024-12-01 12:57:52 -0300

    fusion en una pantalla

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,13 @@
 <script lang="ts">
+    import '../input.css';  // Asegúrate de que esta ruta sea correcta
     import { onMount, afterUpdate } from 'svelte';
 
-    let showForm = true;
     let messages = [];
     let userMessage = '';
     let selectedPatient = '';
-    let loadingMessage = false;
-    let patientFileContent = '';
-    let isFirstMessage = true; // Nueva variable para controlar el scroll inicial
+    let isFirstMessage = true;
     let messagesContainer;
-    let chatContainer;
-    let pageScrollPosition = 0; // Nueva variable para mantener la posición de la página
-
+    let patientFileContent = ''; // Añadida esta línea
 
     let patients = [
         { label: 'Legajo 1: Antonio Demo', value: 'demo1-ejemplo-fisioterapia.txt' },
@@ -20,301 +16,174 @@
     ];
 
     let userName = 'Usuario';
-    let birthChartReport = '';
 
     async function loadPatientFile() {
-        console.log('Función loadPatientFile llamada');
-        console.log('Paciente seleccionado:', selectedPatient);
-
         if (selectedPatient === 'demo1-ejemplo-fisioterapia.txt') {
             try {
                 const response = await fetch('/demo1-ejemplo-fisioterapia.txt');
-                if (response.ok) {
-                    patientFileContent = await response.text();
-                    console.log('Contenido del legajo cargado:', patientFileContent);
-                } else {
-                    console.error('Error al cargar el archivo del legajo');
-                    patientFileContent = 'Error al cargar el archivo de legajo.';
-                }
+                patientFileContent = response.ok
+                    ? await response.text()
+                    : 'Error al cargar el archivo de legajo.';
             } catch (error) {
-                console.error('Error al cargar el archivo:', error);
                 patientFileContent = 'Error al conectar con el servidor para cargar el archivo.';
             }
         } else {
             patientFileContent = '';
-            console.log('Legajo vacío seleccionado, contenido limpiado');
-        }
-    }
-
-    function validateForm() {
-        if (!selectedPatient) {
-            alert('Por favor, selecciona un legajo médico.');
-            return false;
-        }
-        return true;
-    }
-
-    // Función para guardar la posición actual de la página
-    function saveScrollPosition() {
-        pageScrollPosition = window.scrollY;
-    }
-
-    // Función para restaurar la posición de la página
-    function restoreScrollPosition() {
-        window.scrollTo(0, pageScrollPosition);
-    }
-
-    async function startChat() {
-        if (!validateForm()) return;
-
-        saveScrollPosition(); // Guardamos la posición actual
-        loadingMessage = true;
-
-        try {
-            const response = await fetch('/api/loaddocument', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ legajo: selectedPatient })
-            });
-
-            if (!response.ok) {
-                throw new Error('Error al cargar el documento');
-            }
-
-            const result = await response.json();
-            if (result.success) {
-                messages = [
-                    { role: 'assistant', content: 'Bienvenido al asistente médico virtual. ¿En qué puedo asistir en esta historia clínica?' }
-                ];
-                showForm = false;
-                isFirstMessage = true;
-                
-                // Esperamos al siguiente ciclo para restaurar la posición
-                setTimeout(() => {
-                    restoreScrollPosition();
-                }, 0);
-            } else {
-                alert('Error al cargar el documento del legajo.');
-            }
-        } catch (error) {
-            console.error('Error al iniciar la consulta:', error);
-            alert('Hubo un error al iniciar la consulta. Inténtalo de nuevo.');
-        } finally {
-            loadingMessage = false;
         }
     }
 
     async function sendMessage() {
         if (!userMessage.trim()) return;
-
-        saveScrollPosition(); // Guardamos la posición actual de la página
         messages = [...messages, { role: 'user', content: userMessage }];
-        
+        userMessage = '';
         try {
             const response = await fetch('/api/verify', {
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
+                headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
                     user: userName,
                     question: userMessage,
-                    token: 'mysecrettoken',
                     history: messages,
                     legajo: selectedPatient
                 })
             });
-
-            if (response.ok) {
-                const responseData = await response.json();
-                messages = [...messages, { role: 'assistant', content: responseData.message }];
-            } else {
-                const errorData = await response.json();
-                console.error('Error fetching data:', errorData.error);
-                messages = [...messages, { role: 'assistant', content: 'Lo siento, hubo un error al procesar tu mensaje: ' + errorData.error }];
-            }
+            const responseData = await response.json();
+            messages = [...messages, { role: 'assistant', content: responseData.message }];
         } catch (error) {
-            console.error('Error fetching data:', error);
-            messages = [...messages, { role: 'assistant', content: 'Lo siento, hubo un error de conexión. Por favor, verifica tu conexión e intenta nuevamente.' }];
+            messages = [...messages, { role: 'assistant', content: 'Error procesando el mensaje.' }];
         }
-
-        userMessage = '';
         isFirstMessage = false;
     }
 
-    onMount(() => {
-        document.addEventListener('keypress', handleKeyPress);
-        return () => {
-            document.removeEventListener('keypress', handleKeyPress);
-        };
-    });
-
-    afterUpdate(() => {
-        if (messagesContainer && !isFirstMessage) {
-            // Solo hacemos scroll en el contenedor de mensajes, no en toda la página
-            messagesContainer.scrollTop = messagesContainer.scrollHeight;
-            
-            // Restauramos la posición de la página
-            restoreScrollPosition();
-        }
-    });
-
-    function handleKeyPress(event: KeyboardEvent) {
-        if (event.key === 'Enter' && !showForm) {
-            sendMessage();
-        }
+    function handleSuggestion(suggestion: string) {
+    userMessage = suggestion;
+    sendMessage();
     }
 
 
-    let sugerencias = [
-        '¿Desde cuándo presenta el paciente dolor en el hombro izquierdo, y cuál ha sido la evolución de los síntomas?',
-        '¿Existen antecedentes traumáticos, intervenciones previas o factores que puedan predisponer al paciente a esta condición?',
-        '¿Qué pruebas se han realizado para evaluar el dolor en el hombro y los síntomas asociados?',
-        '¿Qué factores en la historia clínica podrían haber agravado el dolor tras la realización de ejercicios como flexiones?',
-        '¿Cuáles son los hallazgos clínicos principales en la movilidad y el examen físico del hombro afectado?',
-        '¿Cuál es el diagnóstico inicial para el dolor de hombro, y qué hipótesis clínicas se están considerando?',
-        '¿Cuál es el plan terapéutico indicado actualmente para el dolor del paciente, y qué objetivos se esperan lograr?',
-        '¿Hay síntomas neurológicos asociados, como adormecimiento o pérdida de sensibilidad en la extremidad afectada?',
-        '¿Qué tipo de imágenes o pruebas adicionales están contempladas para este caso si no hay mejoría clínica?',
-        '¿Cuándo está programada la próxima revisión de seguimiento para evaluar la respuesta al tratamiento?'
-    ];
+    afterUpdate(() => {
+        if (messagesContainer && !isFirstMessage) {
+            messagesContainer.scrollTop = messagesContainer.scrollHeight;
+        }
+    });
 </script>
-
-
 
 <svelte:head>
     <title>Asistente Médico Virtual</title>
     <meta name="description" content="Chat de consulta médica" />
 </svelte:head>
 
-<section>
-    <h1>Asistente Médico Virtual</h1>
-
-    {#if showForm}
-        <div class="form-container">
-            <div class="input-container">
-                <label for="patient">Selecciona un legajo médico:</label>
-                <select id="patient" bind:value={selectedPatient} on:change={loadPatientFile}>
-                    <option value="" disabled selected>-- Selecciona un legajo --</option>
-                    {#each patients as patient}
-                        <option value={patient.value}>{patient.label}</option>
-                    {/each}
-                </select>
-            </div>
-
-            {#if patientFileContent}
-                <div class="input-container">
-                    <label>Detalles del Legajo:</label>
-                    <!-- Textarea para mostrar el contenido de la historia clínica -->
-                    <textarea readonly rows="10" bind:value={patientFileContent}></textarea>
+<div class="w-full h-screen bg-gray-100 flex items-center justify-center">
+    <div class="container mx-auto">
+        <div class="flex border border-grey rounded shadow-lg h-[600px]">
+            <!-- Left Panel -->
+            <div class="w-1/3 border flex flex-col bg-white">
+                <div class="py-2 px-3 bg-gray-100 flex flex-row justify-between items-center">
+                    <h2 class="text-lg font-semibold text-gray-700">Legajos</h2>
                 </div>
-            {/if}
 
+                <div class="py-2 px-3 bg-white">
+                    <select 
+                        class="w-full px-2 py-2 text-sm border rounded" 
+                        bind:value={selectedPatient} 
+                        on:change={loadPatientFile}
+                    >
+                        <option value="" disabled selected>-- Selecciona un legajo --</option>
+                        {#each patients as patient}
+                            <option value={patient.value}>{patient.label}</option>
+                        {/each}
+                    </select>
+                </div>
 
-            <button on:click={startChat}>Iniciar consulta</button>
-            {#if loadingMessage}
-                <p>Iniciando consulta...</p>
-            {/if}
-        </div>
-    {:else}
-        <div class="chat-container">
-            <div class="messages" bind:this={messagesContainer}>
-                {#each messages as message}
-                    <div class={message.role === 'assistant' ? 'assistant-message' : 'user-message'}>
-                        {message.role === 'assistant' ? 'Asistente: ' : 'Tú: '}{message.content}
+                {#if patientFileContent}
+                    <div class="px-3 py-2">
+                        <label class="block text-sm font-medium text-gray-700 mb-1">Detalles del Legajo:</label>
+                        <textarea 
+                            readonly 
+                            rows="10" 
+                            class="w-full px-2 py-2 text-sm border rounded" 
+                            bind:value={patientFileContent}
+                        ></textarea>
                     </div>
-                {/each}
+                {/if}
             </div>
 
-            <div class="input-container">
-                <input type="text" bind:value={userMessage} placeholder="Escribe tu mensaje aquí..." />
-                <button on:click={sendMessage}>Enviar</button>
-            </div>
+            <!-- Right Panel -->
+            <div class="w-2/3 border flex flex-col">
+                <div class="py-2 px-3 bg-gray-100 flex flex-row justify-between items-center">
+                    <div class="flex items-center">
+                        <p class="text-gray-700 font-semibold">Chat Médico</p>
+                    </div>
+                </div>
 
-            <ul class="sugerencias-lista">
-                {#each sugerencias as sugerencia}
-                <li>{sugerencia}</li>
-                {/each}
-            </ul>
+                <!-- Messages Area -->
+                <div 
+                    class="flex-1 overflow-auto p-3" 
+                    style="background-color: #DAD3CC"
+                    bind:this={messagesContainer}
+                >
+                    {#each messages as message}
+                        <div 
+                            class="flex mb-2 {message.role === 'user' ? 'justify-end' : ''}"
+                        >
+                            <div 
+                                class="rounded py-2 px-3 {message.role === 'user' ? 'bg-green-100' : 'bg-gray-200'}"
+                            >
+                                <p class="text-sm">
+                                    {message.role === 'assistant' ? 'Asistente: ' : 'Tú: '}
+                                    {message.content}
+                                </p>
+                            </div>
+                        </div>
+                    {/each}
+                </div>
+
+                <!-- Input Area -->
+                <div class="bg-gray-200 px-4 py-4 flex flex-col">
+                    <!-- Botones de Sugerencia -->
+                    <div class="flex space-x-2 mb-2">
+                        <button 
+                            style="background-color: #3b82f6; color: white; padding: 0.5rem 1rem; border-radius: 9999px; box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);"
+                            on:click={() => handleSuggestion("RESUMEN DEL HISTORIAL")}
+                        >
+                            Resumen del historial
+                        </button>
+                        <button 
+                            class="bg-blue-500 text-white px-3 py-2 rounded-full shadow-md hover:bg-blue-600 transition"
+                            on:click={() => handleSuggestion("SUGERENCIAS DE DIAGNÓSTICO")}
+                        >
+                            Sugerencias de diagnóstico
+                        </button>
+                        <button 
+                            class="bg-blue-500 text-white px-3 py-2 rounded-full shadow-md hover:bg-blue-600 transition"
+                            on:click={() => handleSuggestion("SUGERIR TRATAMIENTO")}
+                        >
+                            Sugerir tratamiento
+                        </button>
+                    </div>
+
+
+                    <!-- Input de Texto y Botón de Enviar -->
+                    <div class="flex items-center">
+                        <div class="flex-1 mr-4">
+                            <input 
+                                class="w-full border rounded px-2 py-2" 
+                                type="text" 
+                                placeholder="Escribe tu mensaje aquí..."
+                                bind:value={userMessage}
+                            />
+                        </div>
+                        <div>
+                            <button 
+                                class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition"
+                                on:click={sendMessage}
+                            >
+                                Enviar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    {/if}
-</section>
-
-<style>
-    section {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        padding: 0px 20px 20px 20px;
-    }
-
-    h1 {
-        margin-bottom: 20px;
-        font-family: 'Poppins', sans-serif;
-        font-size: 2.5rem;
-        font-weight: 600;
-        text-align: center;
-    }
-
-    .form-container, .chat-container {
-        width: 100%;
-        max-width: 1000px;
-        background: rgba(255, 255, 255, 0.9);
-        padding: 20px;
-        border-radius: 10px;
-    }
-
-    .messages {
-        width: 100%;
-        height: 200px;
-        margin-bottom: 20px;
-        padding: 10px;
-        overflow-y: auto;
-        border: 1px solid #ccc;
-        background: #fff;
-    }
-
-    .assistant-message {
-        color: blue;
-        margin-bottom: 10px;
-    }
-
-    .user-message {
-        color: green;
-        margin-bottom: 10px;
-        text-align: right;
-    }
-
-    .input-container {
-        display: flex;
-        flex-direction: column;
-        width: 100%;
-        margin-bottom: 10px;
-    }
-
-    label {
-        margin-bottom: 5px;
-    }
-
-    select, input, textarea {
-        flex: 1;
-        padding: 10px;
-        margin-bottom: 10px;
-    }
-
-    button {
-        padding: 10px;
-        background-color: #4CAF50;
-        color: white;
-        border: none;
-        cursor: pointer;
-        transition: background-color 0.3s;
-    }
-
-    button:hover {
-        background-color: #45a049;
-    }
-</style>
+    </div>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,svelte,js,ts}', // Rango de archivos a escanear para las clases de Tailwind
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
